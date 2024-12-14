0a4882675c103be1275e1dfd50c13372c94636e1
Bulhakov Danylo <glukoza142@gmail.com>
2024-12-14 23:46:12 +0200

    - got some work on UI done, moved some files around but thats about it

--- Project_OOP_WPF/MainWindow.xaml.cs
+++ Project_OOP_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Project_OOP_WPF.UserControls;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,31 @@ namespace Project_OOP_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Hospital> _hospitals = new List<Hospital>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NewHospital_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new NewHospitalView();
+        }
+
+        private void ViewHospitals_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new ViewHospitalsView();
+        }
+
+        private void Patients_AddPatientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new AddPatientView();
+        }
+
+        private void Patients_ViewPatients_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new ViewPatientsView();
         }
     }
 }
