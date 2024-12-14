f8565104760ee498147ae690fab943e334c739f8
Bulhakov Danylo <glukoza142@gmail.com>
2024-12-14 02:50:37 +0200

    - need to fix tests ASAP

--- Project_OOP_WPF/Hospital.cs
+++ Project_OOP_WPF/Hospital.cs
@@ -12,32 +12,60 @@ namespace Project_OOP_WPF
 {
     public class Hospital
     {
+        private string _name;
+        private string _location;
         #region Properties
-        public int ID
+        public int ID { get; private set; }
+        public static IDManagement IDManager = new IDManagement();
+        public string Name 
         {
-            get { throw new NotImplementedException(); }
-            private set { throw new NotImplementedException(); }
+            get => _name;
+            set
+            {
+                _name = ChangeNameOrLocation(value);
+            } 
         }
-        public IDManagement IDManager = new IDManagement();
-        public string Name { get; set; }
-        public string Location { get; set; }
+        public string Location
+        {
+            get => _location;
+            set
+            {
+                _location = ChangeNameOrLocation(value);
+            }
+        }
 
-        public List<Department> Departments
-        {
-            get { throw new NotImplementedException(); }
-            private set { throw new NotImplementedException(); }
-        }
+        public List<Department> Departments { get; private set; } = new List<Department>();
         public SortedSet<int> Rooms { get; set; } = new();
         public List<Person> People { get; set; } // both patients and staff should be stored here
         // 
-        public List<Patient> Patients { get { return People.OfType<Patient>().ToList(); } }
-        public List<Staff> ActiveStaff { get { return People.OfType<Staff>().ToList(); } }
+        public List<Patient> Patients => People.OfType<Patient>().ToList();
+        public List<Staff> ActiveStaff => People.OfType<Staff>().ToList();
         #endregion
 
         #region Methods
-        public Hospital(string name, string location, List<int> rooms, List<Department>? departments = null, List<Patient>? patient = null, List<Staff>? activeStaff = null)
-        { throw new NotImplementedException(); }
-        public void ChangeName(string newName)
+        public Hospital(string name, string location, SortedSet<int> rooms, List<Department>? departments = null, List<Patient>? patients = null, List<Staff>? activeStaff = null)
+        {
+            List<string> exceptions = new();
+
+            try { Name = name; }
+            catch(Exception ex) { exceptions.Add(ex.Message); }
+
+            try { Location = location; }
+            catch (Exception ex) { exceptions.Add(ex.Message); }
+
+            Rooms = rooms;
+
+            Departments = departments ?? new List<Department>();
+
+            if (patients != null) People.AddRange(patients);
+            if (activeStaff != null) People.AddRange(activeStaff);
+
+            if(exceptions.Count > 0)
+                throw new ExceptionList(exceptions);
+
+            ID = IDManager.GenerateID();
+        }
+        public string ChangeNameOrLocation(string newName)
         {
             if (string.IsNullOrWhiteSpace(newName))
                 throw new NullReferenceException("! HOSPITAL: Cannot have an empty name.");
@@ -46,14 +74,15 @@ namespace Project_OOP_WPF
             if (newName.Length > 50)
                 throw new ArgumentException("! HOSPITAL: Cannot have a name that is longer than 50 symbols.");
             // use regex to check for invalid symbols
-            Regex regex = new(@"^[a-zA-Z '-]+$");
+            Regex regex = new(@"^[a-zA-Z0-9 '-]+$");
             if (!regex.IsMatch(newName))
                 throw new ArgumentException("! HOSPITAL: Illegal symbols detected.");
+            return newName;
         }
         #endregion
 
         #region Methods - Room-related
-        private bool AddRoom(int roomID)
+        public bool AddRoom(int roomID)
         {
             if (roomID < 1) throw new ArgumentException("! HOSPITAL: Room ID cannot be lower than 1.");
             if(!Rooms.Add(roomID)) throw new ArgumentException($"! HOSPITAL: Room {roomID} already exists.");
