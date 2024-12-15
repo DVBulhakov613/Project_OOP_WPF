3ff1e40b2f0d45d3b93e9d51fd6fa69052e3fbc8
Bulhakov Danylo <glukoza142@gmail.com>
2024-12-15 02:59:24 +0200

    - done some basic interface implementation

--- Project_OOP_WPF/MainWindow.xaml.cs
+++ Project_OOP_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Project_OOP_WPF.UserControls;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,31 +18,37 @@ namespace Project_OOP_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Hospital> _hospitals = new List<Hospital>();
+        public ObservableCollection<Hospital> Hospitals = new();
 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
-        private void NewHospital_Click(object sender, RoutedEventArgs e)
+        private void HospitalWindow_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new NewHospitalView();
+            MainContent.Content = new CreateHospital(this);
         }
 
         private void ViewHospitals_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new ViewHospitalsView();
+            MainContent.Content = new CreateHospital(this);
         }
 
         private void Patients_AddPatientsButton_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new AddPatientView();
+            //MainContent.Content = new AddPatientView();
         }
 
         private void Patients_ViewPatients_Click(object sender, RoutedEventArgs e)
         {
-            MainContent.Content = new ViewPatientsView();
+            //MainContent.Content = new ViewPatientsView();
+        }
+
+        private void AddAppointment_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new CreateAppointments();
         }
     }
 }
